c12435c1835e3ba143a44f5c69177624982166df
jiaxiwang331 <77806665+jiaxiwang331@users.noreply.github.com>
2022-01-26 21:02:31 -0600

    a1

--- A1/WordSearch.py
+++ A1/WordSearch.py
@@ -23,23 +23,37 @@ import sys
 # Input: None
 # Output: function returns a 2-D list that is the grid of letters and
 #         1-D list of words to search
-def read_input ( ):
+def read_input():
+  n = int(float(sys.stdin.readline().strip()))
+  n1 = sys.stdin.readline()
+  for row in range(n):
+    r = sys.stdin.readline()
+    return r
+
+
+
+  # for line in sys.stdin:
+  #   print(line)
+
+read_input()
+
 
 # Input: a 2-D list representing the grid of letters and a single
 #        string representing the word to search
 # Output: returns a tuple (i, j) containing the row number and the
 #         column number of the word that you are searching 
 #         or (0, 0) if the word does not exist in the grid
-def find_word (grid, word):
+# def find_word (grid, word):
 
-def main():
-  # read the input file from stdin
-  word_grid, word_list = read_input()
 
-  # find each word and print its location
-  for word in word_list:
-    location = find_word (word_grid, word)
-	print (word + ": " + str(location))
+# def main():
+#   # read the input file from stdin
+#   word_grid, word_list = read_input()
 
-if __name__ == "__main__":
-  main()
+#   # find each word and print its location
+#   for word in word_list:
+#     location = find_word (word_grid, word)
+#   print (word + ": " + str(location))
+
+# if __name__ == "__main__":
+#   main()
