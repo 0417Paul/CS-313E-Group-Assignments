af5db9f96b6d4850ed4fb23b8c130de640628e18
jiaxiwang331 <77806665+jiaxiwang331@users.noreply.github.com>
2022-01-27 15:04:53 -0600

    search row

--- A1/WordSearch.py
+++ A1/WordSearch.py
@@ -35,7 +35,7 @@ def read_input():
 
   #read the grid into a list
   for row in range(n):
-    r = sys.stdin.readline().strip().split(" ")
+    r = sys.stdin.readline().strip().upper().split(" ")
     word_grid.append(r)
 
   # skip the blank line
@@ -44,7 +44,7 @@ def read_input():
   # read the word list
   k = int(sys.stdin.readline().strip())
   for ki in range(k):
-    i = sys.stdin.readline().strip()
+    i = sys.stdin.readline().strip().upper()
     word_list.append(i)
   
   return word_grid, word_list
@@ -64,26 +64,30 @@ def read_input():
 def find_word (grid, word):
   i = 1
   j = 1
-  for g in grid:  
-    for gl in range(len(g)): 
-      g_index = gl
-      for w in word:
-        while word.index(w) <= len(word)-1 and g_index <= len(g)-1:      
-          if w in g:                
-            if w == g[g_index]:
+  for w in range(len(word)):
+    # row search
+    for g in grid:  
+      if word[w] in g:
+        for gl in range(len(g)): 
+          g_index = gl
+          w_index = w
+          while w_index <= len(word)-1 and g_index <= len(g)-1:                               
+            if word[w_index] == g[g_index]:
               #print(g[g_index])
-              
-              if word.index(w) == len(word) - 1:                
+              if w_index == len(word) - 1: 
+                #print(tuple([i, g_index]))               
                 break
+              w_index += 1
               g_index += 1
-                
-            elif w != g[g_index]:
+                          
+            elif word[w] != g[g_index]:
               break 
-      print(tuple([i, g.index(w)]))
     #i += 1
-
+  print(tuple([i, g_index]))
+      #print(tuple([i, g.index(w)]))
+            
      
-find_word (['S', 'I', 'N', 'S', 'N', 'O', 'W', 'S', 'T', 'O', 'R', 'M', 'W', 'I'], "SNOW")
+find_word ([['S', 'I', 'N', 'S', 'N', 'O', 'W', 'S', 'T', 'O', 'R', 'M', 'W', 'I'],['K', 'L', 'N', 'T', 'S', 'N', 'O', 'W', 'M', 'A', 'N', 'E', 'F', 'E']], "SNOW")
 
 # def main():
 #   # read the input file from stdin
