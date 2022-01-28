bb36f11642db20aa59a5ce8643ff159723444974
0417Paul <89763823+0417Paul@users.noreply.github.com>
2022-01-28 00:10:32 -0600

    Created A2 Folder and started formating the python file

---
+++ A2/Intervals.py
@@ -0,0 +1,57 @@
+#  File: WordSearch.py
+
+#  Description: take a set of intervals and collapse all the overlapping intervals 
+#  and print the smallest set of non-intersecting intervals in ascending order of the 
+#  lower end of the interval and then print the intervals in increasing order of the size of the interval.
+
+#  Student Name:  Yilin Wen
+
+#  Student UT EID: YW22559
+
+#  Partner Name: Jiaxi Wang
+
+#  Partner UT EID: JW53499
+
+#  Course Name: CS 313E 
+
+#  Unique Number: 51120
+
+#  Date Created: 01/27/2022
+
+#  Date Last Modified: 1/31/2022
+
+import sys
+
+# Input: tuples_list is an unsorted list of tuples denoting intervals
+# Output: a list of merged tuples sorted by the lower number of the
+#         interval
+def merge_tuples (tuples_list):
+  pass
+
+# Input: tuples_list is a list of tuples of denoting intervals
+# Output: a list of tuples sorted by ascending order of the size of
+#         the interval
+#         if two intervals have the size then it will sort by the
+#         lower number in the interval
+def sort_by_interval_size (tuples_list):
+  pass
+
+
+def main():
+  # read the input data and create a list of tuples
+
+  # merge the list of tuples
+
+  # print the merged list
+
+  # sort the list of tuples according to the size of the interval
+
+  # print the sorted list
+  pass
+
+
+
+
+
+if __name__ == "__main__":
+  main()
